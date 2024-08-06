28ea85aded5053a49e4eadcdc370d5a5eb7b6f32
Matthew McCall <m-mccall+git@outlook.com>
2024-08-06 14:51:47 -0400

    Switch to SCSS and integrate Bootstrap
    Changed global styles from CSS to SCSS for better pre-processing support. Added Bootstrap and React-Bootstrap for enhanced UI components. Updated main layout and home page to utilize Bootstrap classes for improved styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import {Container, Stack} from "react-bootstrap";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Open Algebra Project</h1>
-      <p>The Open Algebra Project is dedicated to democratizing access to powerful mathematical tools by providing free, open-source, and accessible computer algebra software. Our mission is to empower individuals, educators, and researchers worldwide by eliminating barriers to advanced mathematical computation and fostering a collaborative community. Through innovation, inclusivity, and transparency, we strive to enhance mathematical education, facilitate groundbreaking research, and inspire a passion for mathematics in people of all backgrounds.</p>
-    </main>
-  );
+    return (
+        <main>
+            <Stack className="min-vh-100 justify-content-center">
+                <Container>
+                    <h1 className={"fw-semibold display-1"}>Open Algebra Project</h1>
+                    <p className={"lead"}>The Open Algebra Project is dedicated to democratizing access to powerful mathematical tools by
+                        providing free, open-source, and accessible computer algebra software. Our mission is to empower
+                        individuals, educators, and researchers worldwide by eliminating barriers to advanced mathematical
+                        computation and fostering a collaborative community. Through innovation, inclusivity, and
+                        transparency, we strive to enhance mathematical education, facilitate groundbreaking research, and
+                        inspire a passion for mathematics in people of all backgrounds.</p>
+                </Container>
+            </Stack>
+        </main>
+    );
 }
