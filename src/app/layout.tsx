68aa0a7da6e1466d20c06353c349aff2070dfe1f
Matthew McCall <m-mccall+git@outlook.com>
2024-08-15 18:28:29 -0400

    Integrate Microsoft Clarity tracking script
    Added Microsoft Clarity script to the layout component to enable user session tracking. Updated the workflow to include the Clarity project ID from GitHub secrets for the build environment.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+        `}
+      </Script>
     </html>
   );
 }
