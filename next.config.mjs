53ffe3c6a804b5eef7c54750a3843a9445080d42
Matthew McCall <m-mccall+git@outlook.com>
2024-08-16 08:39:19 -0400

    Add redirect from '/webapp' to '/app' in next.config.mjs
    Implemented a permanent redirect to ensure users accessing the old '/webapp' route are redirected to the new '/app' route. This change accounts for the project's evolution from a React Vite project to its current structure.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    typescript: {
+    output: 'export', typescript: {
         ignoreBuildErrors: true,
+    }, async redirects() {
+        return [{
+            /*
+            Oasis Web was originally a React Vite project called 'webapp' which had the GitHub pages route of `/webapp'
+            this has now been replaced by the '/app' route in this repository.
+             */
+            source: '/webapp', destination: '/app', permanent: true,
+        }]
     }
 };
 
