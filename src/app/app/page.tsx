bf0f9ce2cdcaa4d1d01d9a6aa7f577d674074a2a
Matthew McCall <m-mccall+git@outlook.com>
2024-08-29 13:02:07 -0400

    Refactor dynamic import for OasisC module
    Replaced static import with a dynamic import for the OasisC module to improve performance and ensure on-demand loading. This change uses the default export from the dynamically imported module and integrates it into the existing useEffect hook.

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -3,11 +3,15 @@
 import {useEffect, useState} from "react";
 import Loading from "@/app/app/loading";
 import App from "@/app/app/app";
-import loadOasis from "./OasisC.js"
 
 export default function AppPage() {
     const [oasis, setOasis] = useState();
-    useEffect(() => { loadOasis().then(oasis => setOasis(oasis)); }, [])
 
-    return oasis ? <App oasis={oasis} /> : <Loading />
+    useEffect(() => {
+        import("./OasisC")
+            .then(loadOasis => loadOasis.default())
+            .then(oasis => setOasis(oasis));
+    }, []);
+
+    return oasis ? <App oasis={oasis} /> : <Loading />;
 }
